63861a45e726a900ec08d53006f7303422ea7b35
alyesque <alysonescalante@gmx.com>
2021-11-11 12:11:40 -0800

    added dynamic routing
    added dynamic routing

---
+++ components/ArticleList.js
@@ -0,0 +1,42 @@
+import { parse } from 'node-html-parser'
+import Link from 'next/link'
+
+const ArticleList = ({ articles }) => {
+
+    function parseImg(x) {
+        const root = parse(x);
+
+        return root.childNodes[3].childNodes[0].rawAttrs.substring(6, 249)
+
+    }
+
+    function parsePreview(x) {
+        const root = parse(x);
+        return root.childNodes[0].childNodes[0]._rawText
+    }
+    return (
+        <div>
+            <div>
+                <div className='subjectheader'>Recent Posts</div>
+                <div className='item-container'>
+                    {articles.map((post) => {
+                        return (
+                            <div className='cardSmall' key={post.id}>
+                                <div className='left'> <img src={parseImg(post.content)} alt='image' /><br /> </div>
+                                <div className='right'>
+                                    <h1>{post.title}</h1>
+
+                                    <p> {parsePreview(post.content)} </p>
+
+                                    <div className='sec'><Link href='/article/[id]' as={'/article/' + articles.indexOf(post)}><button>Read More</button></Link></div>
+                                </div>
+
+                            </div>
+                        )
+                    })}
+                </div>
+            </div >
+        </div>
+    )
+}
+export default ArticleList
---
+++ pages/article/[id]/index.js
@@ -0,0 +1,32 @@
+import { useRouter } from "next/dist/client/router"
+
+const article = ({ articles }) => {
+    const router = useRouter()
+    const { id, content } = router.query
+    console.log(articles[id])
+    return (
+
+        <div className='card' key={articles[id]}>
+
+
+            <h1> {articles[id].title} </h1>
+            <div className="blogPost" dangerouslySetInnerHTML={{ __html: articles[id].content }} />
+
+        </div>
+    )
+}
+
+export const getServerSideProps = async (context) => {
+    const res = await fetch('https://www.googleapis.com/blogger/v3/blogs/6835549372255884484/posts?key=AIzaSyCgmIgM0Ld_eI6FdUNxqDT_HHOGkh8MH-M&fbclid=IwAR35jRH4P8wmjbiNsilxV1rnWwdrjgJPEE9hJVSx85IB6DGx8BEJXVqhim0')
+    const results = await res.json()
+    let articles = results.items
+
+
+    return {
+        props: {
+            articles
+        }
+    }
+}
+
+export default article
