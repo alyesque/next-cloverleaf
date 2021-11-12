73ad3b7321695b40b1ccec5db034eaa1e9f89ed0
alyesque <alysonescalante@gmx.com>
2021-11-11 17:03:16 -0800

    crossOrigin

--- pages/_app.js
+++ pages/_app.js
@@ -8,10 +8,10 @@ function MyApp({ Component, pageProps }) {
     <div>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@900&display=swap" rel="stylesheet" />
       </Head>
       <Navigation />
