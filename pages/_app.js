b4292d5cdacb1ea0587586884a1b4356c41b2292
alyesque <alysonescalante@gmx.com>
2021-11-11 16:59:48 -0800

    font

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import '../styles/globals.css'
 import Navigation from '../components/Navigation'
 import Footer from '../components/Footer'
+import Head from 'next/dist/shared/lib/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@900&display=swap" rel="stylesheet" />
+      </Head>
       <Navigation />
       <Component {...pageProps} />
       <Footer />
