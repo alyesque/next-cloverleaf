46a3668186e38155c67988bbfe9443dcf58b0e83
alyesque <89320257+alyesque@users.noreply.github.com>
2021-11-15 12:22:16 -0800

    test

---
+++ components/Testimonials.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+function Testimonials() {
+  let t1 =
+    "“Clare is an amazing tutor, she is kind, patient and very knowledgeable in mathematics. She has helped me to understand the material in school, as well as preparing me for exams. She crafts interesting material with real world examples. I greatly enjoy my time spent working with her.” ";
+  let s1 = "AJ, 9th grade (AP statistics)";
+
+  let t2 =
+    "“Clare worked with me in preparation for the GRE. Math has always left me feeling insecure, but Clare’s infectious enthusiasm for the subject made the commitment of time to studying much less intimidating. Her deep knowledge, patience, and abundant assurance put me at my best for test day.” ";
+  let s2 = "Alex T, (GRE prep)";
+  const [student, setStudent] = useState(s1);
+  const [testim, setTestim] = useState(t1);
+
+  function changeTest() {
+    console.log(testim);
+    if (
+      testim ===
+      "“Clare is an amazing tutor, she is kind, patient and very knowledgeable in mathematics. She has helped me to understand the material in school, as well as preparing me for exams. She crafts interesting material with real world examples. I greatly enjoy my time spent working with her.” "
+    ) {
+      setStudent(s2);
+      setTestim(t2);
+      console.log("clicked");
+    } else if (
+      testim ===
+      "“Clare worked with me in preparation for the GRE. Math has always left me feeling insecure, but Clare’s infectious enthusiasm for the subject made the commitment of time to studying much less intimidating. Her deep knowledge, patience, and abundant assurance put me at my best for test day.” "
+    ) {
+      setStudent(s1);
+      setTestim(t1);
+      console.log("clicked");
+    }
+  }
+
+  return (
+    <div className="testimonial">
+      <div className="clicker" onClick={() => changeTest()}>
+        {" "}
+        ←
+      </div>
+      <div className="testiCard">
+        <p>{testim}</p>
+        <h3>{student}</h3>
+      </div>
+      <div className="clicker" onClick={() => changeTest()}>
+        {" "}
+        →{" "}
+      </div>
+    </div>
+  );
+}
+
+export default Testimonials;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-import Head from 'next/head'
-import Link from 'next/link'
-
-
+import Head from "next/head";
+import Link from "next/link";
+import Testimonials from "../components/Testimonials";
 
 export default function Home() {
   return (
@@ -12,15 +11,21 @@ export default function Home() {
       <main>
         <section className="flex landing">
           <div className="left1">
-            <h1 className='h1Big'>
-              Private Math Lessons</h1>
-            <p> Keep your math skills sharp! I work with K-12 students, college students, and adult learners to help them achieve their goals. I can help with homework and class content, or I can design a custom learning plan for your student. Both online and in-person sessions are available.
+            <h1 className="h1Big">Private Math Lessons</h1>
+            <p>
+              {" "}
+              Keep your math skills sharp! I work with K-12 students, college
+              students, and adult learners to help them achieve their goals. I
+              can help with homework and class content, or I can design a custom
+              learning plan for your student. Both online and in-person sessions
+              are available.
             </p>
-            <Link href='/contact'><button>  Request A Consultation </button></Link>
-
+            <Link href="/contact">
+              <button> Request A Consultation </button>
+            </Link>
           </div>
           <div className="right1">
-            <img src='./images/testart.png' className="sectionimage" />
+            <img src="./images/testart.png" className="sectionimage" />
           </div>
         </section>
         {/* <section className="about">
@@ -28,39 +33,50 @@ export default function Home() {
           <img src='./images/team.png' className="medimg" />
           <p> At Cloverleaf Math Center, we really love math.We also know firsthand that a one-size-fits-all approach to math education just doesn't work. We're constantly brainstorming new ways to explain material & consulting new research in mathematics education and curriculum design.We work with students with many different goals, and we know how to be responsive to a student's unique needs and learning style. We specialize in working with high school and college students, as well as adult learners and younger students looking for math enrichment. </p>
         </section> */}
-        <hr className='hrHome' />
+        <hr className="hrHome" />
         <section className="flex list">
           <div className="left1">
             <h2> Homework Help </h2>
-            <img src='./images/meeting.png' className="smallimg" />
-            <p> In a one-on-one tutoring session, I’ll explain concepts and methods in a way that is compatible with your learning style and background, and guide you through each stage of the learning process. We can identify and correct any subtle misunderstandings so that you have a strong foundation for future work.</p>
+            <img src="./images/meeting.png" className="smallimg" />
+            <p>
+              {" "}
+              In a one-on-one tutoring session, I’ll explain concepts and
+              methods in a way that is compatible with your learning style and
+              background, and guide you through each stage of the learning
+              process. We can identify and correct any subtle misunderstandings
+              so that you have a strong foundation for future work.
+            </p>
           </div>
           <div className="right1">
             <h2> SAT/ACT/GRE Math Prep </h2>
-            <img src='./images/meeting1.png' className="smallimg" />
+            <img src="./images/meeting1.png" className="smallimg" />
             <p>
-              Don't be intimidated by standardized tests. I can help you review important concepts and learn crucial strategies & time-saving tips. I’ll work with you in one-on-one tutoring sessions instead of pre-defined packages or group classes- this lets us personalize every aspect of our work together.
+              Don't be intimidated by standardized tests. I can help you review
+              important concepts and learn crucial strategies & time-saving
+              tips. I’ll work with you in one-on-one tutoring sessions instead
+              of pre-defined packages or group classes- this lets us personalize
+              every aspect of our work together.
             </p>
           </div>
           <div className="right1">
             <h2>Custom Learning Plans</h2>
-            <img src='./images/meeting2.png' className="smallimg" />
-            <p>We can schedule a free assessment to help me design a customized learning plan for your student. Let me help you soar ahead or get caught up! Whether you're looking for enrichment, review, or anything in between, I can help. Schedule your assessment today!</p>
-          </div>
-        </section>
-        <hr className='hrHome' />
-
-        <section className=" flex">
-
-          <img src='./images/testi.png' className="sectionimage2 left1" />
-          <div className='right1'>
-            <h1> Testimonials </h1>
-            <p>“Clare is an amazing tutor, she is kind, patient and very knowledgeable in mathematics. She has helped me to understand the material in school, as well as preparing me for exams. She crafts interesting material with real world examples. I greatly enjoy my time spent working with her.” -AJ, 9th grade (AP statistics)
+            <img src="./images/meeting2.png" className="smallimg" />
+            <p>
+              We can schedule a free assessment to help me design a customized
+              learning plan for your student. Let me help you soar ahead or get
+              caught up! Whether you're looking for enrichment, review, or
+              anything in between, I can help. Schedule your assessment today!
             </p>
           </div>
         </section>
-      </main>
+        <hr className="hrHome" />
 
+        <section className="padsect">
+          <h1>Testimonials</h1>
+
+          <Testimonials />
+        </section>
+      </main>
     </div>
-  )
+  );
 }
